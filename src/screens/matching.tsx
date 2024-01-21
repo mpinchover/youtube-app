8d846552b2fd8cc436aa5d8f83788ad4dd9f1165
matt-pinch <m.pinchover@swordhealth.com>
2024-01-21 12:46:24 -0500

    added in loading new profiles

--- src/screens/matching.tsx
+++ src/screens/matching.tsx
@@ -2,27 +2,73 @@ import { View, Text, StyleSheet } from "@bacons/react-views";
 import ProfilePreview from "../components/discover/profile-preview";
 import { Button } from "@rneui/base";
 import { useState, useRef } from "react";
-import { Animated, Dimensions } from "react-native";
+import { Animated, Dimensions, ActivityIndicator } from "react-native";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+import { matchingCandidates } from "../recoil/matching/matching";
+import axios from "axios";
 
-const profiles = [{}, {}, {}];
 const Matching = () => {
-  const [profilePreviewIdx, setProfilePreviewIdx] = useState(1);
+  const [candidateProfiles, setCandidateProfiles] =
+    useRecoilState(matchingCandidates);
+
+  const [profilePreviewIdx, setProfilePreviewIdx] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const translation = useRef(new Animated.Value(0)).current;
   const width = Dimensions.get("window").width;
 
-  const next = () => {
-    setProfilePreviewIdx((prev) => prev + 1);
+  const next = async () => {
+    // this is the last profile preview that has been loaded up
+    if (profilePreviewIdx === candidateProfiles.length - 1) {
+      try {
+        // set loading screen
+        setIsLoading(true);
+
+        // get candidates
+        const result = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+
+        for (let i = 0; i < 100; i++) {
+          await axios.get("https://jsonplaceholder.typicode.com/comments");
+        }
+
+        // reset values
+        translation.setValue(0);
+        setProfilePreviewIdx(0);
+        setCandidateProfiles([{}, {}, {}]);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
+      return;
+    }
+
     Animated.timing(translation, {
-      toValue: -profilePreviewIdx * width,
+      toValue: -(profilePreviewIdx + 1) * width,
       useNativeDriver: true,
       duration: 300,
     }).start();
+    setProfilePreviewIdx((prev) => prev + 1);
   };
 
   const acceptCandidate = () => {};
 
   const rejectCandidate = () => {};
-
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <Text>Loading candidates</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Animated.View
@@ -33,7 +79,7 @@ const Matching = () => {
           transform: [{ translateX: translation }],
         }}
       >
-        {profiles.map((e, i) => {
+        {candidateProfiles.map((e, i) => {
           return <ProfilePreview key={i} next={next} />;
         })}
       </Animated.View>
@@ -45,6 +91,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
